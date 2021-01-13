a002b2ac4a6a06564a8ce436eb7c33999da7ef03
FrancisBilla <francis.billa.fb@gmail.com>
2021-01-13 10:15:54 +0000

    refactoring path

--- jsonValidation1.go
+++ jsonValidation1.go
@@ -6,8 +6,17 @@ import (
 )
 
 func main() {
-    schemaLoader := gojsonschema.NewReferenceLoader("file:///home/francis/go/src/github.com/turntabl/validation of GoLang/user_schema.json")
-    documentLoader := gojsonschema.NewReferenceLoader("file:///home/francis/go/src/github.com/turntabl/validation of GoLang/document.json")
+    // schemaLoader := gojsonschema.NewReferenceLoader("file:///home/francis/go/src/github.com/turntabl/validation of GoLang/user_schema.json")
+    // documentLoader := gojsonschema.NewReferenceLoader("file:///home/francis/go/src/github.com/turntabl/validation of GoLang/document.json")
+
+    path1 := "/home/francis/go/src/github.com/turntabl/validation of GoLang/user_schema.json"
+    path2 := "/home/francis/go/src/github.com/turntabl/validation of GoLang/document.json"
+
+    schemaLoader := gojsonschema.NewReferenceLoader("file://" + path1)
+    documentLoader := gojsonschema.NewReferenceLoader("file://" + path2)
+
+
+
 
     result, err := gojsonschema.Validate(schemaLoader, documentLoader)
     if err != nil {
